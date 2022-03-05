76cea85528a4ab5a877d34f11937e8ef93957449
rahulbn007 <96820417+rahulbn007@users.noreply.github.com>
2022-03-05 16:12:05 +0000

    .

--- p2original.c
+++ p2original.c
@@ -1,10 +1,32 @@
 #include <stdio.h>
-struct _fraction
+typedef struct _fraction
 {
   int num,den;
 }Fraction;
 Fraction input_fraction()
 {
   Fraction f;
-  printf("")
+  printf("Enter  the first fraction:\n");
+  scanf("%d%d",&f.num,&f.den);
+  return f;
+}
+Fraction Largest_fraction(Fraction f1, Fraction f2, Fraction f3)
+{
+  Fraction L;
+  L=((f1.num/f1.den)>(f2.num/f2.den)?((f1.num/f1.den)>(f3.num/f3.den)?(f1):(f3)):((f2.num/f2.den)>(f3.num/f3.den)?(f2):(f3)));
+  return L;
+}
+void output(Fraction f1, Fraction f2, Fraction f3, Fraction largest)
+{
+  printf("The largest of three fractions : %d/%d %d/%d %d/%d is \n %d/%d\n",f1.num,f1.den,f2.num,f2.den,f3.num,f3.den,largest.num,largest.den);
+}
+int main()
+{
+  Fraction f1,f2,f3,largest;
+  f1=input_fraction();
+  f2=input_fraction();
+  f3=input_fraction();
+  largest=Largest_fraction( f1,f2,f3);
+  output( f1,f2,f3,largest);
+  return 0;
 }
