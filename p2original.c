47f07a9ab4b02130656f6178f7540c397d2b0553
rahulbn007 <96820417+rahulbn007@users.noreply.github.com>
2022-03-05 17:32:51 +0000

    .

--- p2original.c
+++ p2original.c
@@ -12,9 +12,16 @@ Fraction input_fraction()
 }
 Fraction Largest_fraction(Fraction f1, Fraction f2, Fraction f3)
 {
-  Fraction L;
-  L=((f1.num/f1.den)>(f2.num/f2.den)?((f1.num/f1.den)>(f3.num/f3.den)?(f1):(f3)):((f2.num/f2.den)>(f3.num/f3.den)?(f2):(f3)));
-  return L;
+  float x,y,z;
+  x=(float)f1.num/f1.den;
+  y=(float)f2.num/f2.den;
+  z=(float)f3.num/f3.den;
+ if(x<y && x<z)
+   return f1;
+ else if(y<x && y<z)
+   return f2;
+  else
+   return f3;
 }
 void output(Fraction f1, Fraction f2, Fraction f3, Fraction largest)
 {
@@ -26,7 +33,7 @@ int main()
   f1=input_fraction();
   f2=input_fraction();
   f3=input_fraction();
-  largest=Largest_fraction( f1,f2,f3);
-  output( f1,f2,f3,largest);
+  largest=Largest_fraction(f1,f2,f3);
+  output(f1,f2,f3,largest);
   return 0;
 }
